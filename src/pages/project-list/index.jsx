d31b97111b0fd4ad130e92b82fd715ffbb9d0484
tuchg <24775733+tuchg@users.noreply.github.com>
2022-05-07 14:19:27 +0800

    fix: useDebounce未生效问题

--- src/pages/project-list/index.jsx
+++ src/pages/project-list/index.jsx
@@ -3,7 +3,7 @@ import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
 import { List } from "./list";
 import qs from "qs";
-import { cleanObject, useMount } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 
 const apiURL = process.env.REACT_APP_API_URL;
 
@@ -15,15 +15,17 @@ export const ProjectListPage = () => {
   //外部如何获取共享数据，引起状态提升
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+  const debounceParam = useDebounce(param, 2000);
 
   // 监听param改变，触发时重新请求数据
   useEffect(() => {
-    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(async resp => {
+    debugger
+    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async resp => {
       if (resp.ok) {
         setList(await resp.json());
       }
     });
-  }, [param]);
+  }, [debounceParam]);
 
   useMount(() => {
     fetch(`${apiURL}/users`).then(async resp => {
