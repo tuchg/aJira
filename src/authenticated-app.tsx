cf129e29a5f9f2f2aff749ade63625ac708b09e0
tuchg <24775733+tuchg@users.noreply.github.com>
2022-05-08 21:15:18 +0800

    feat: 基于emotion实现的Row组件

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,13 +2,14 @@ import { ProjectListPage } from "./pages/project-list";
 import { useAuth } from "./context/auth-context";
 import { Button } from "antd";
 import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <LeftHeader>
+      <Header between={true}>
+        <LeftHeader gap={true}>
           <h3>Logo</h3>
           <h3>项目</h3>
           <h3>用户</h3>
@@ -31,16 +32,8 @@ const Container = styled.div`
 `;
 
 //grid-area用于起名
-const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const LeftHeader = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const Header = styled(Row)``;
+const LeftHeader = styled(Row)``;
 const RightHeader = styled.div``;
 
 const Main = styled.main``;
