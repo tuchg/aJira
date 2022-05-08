7bc3ca9e9ad518aa845be6e3db4214256f7994b4
tuchg <24775733+tuchg@users.noreply.github.com>
2022-05-08 15:30:43 +0800

    feat: 封装通用HTTP工具

---
+++ src/utils/http.ts
@@ -0,0 +1,38 @@
+import qs from "qs";
+import { logout } from "../auth-provider";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+  return window.fetch(`${apiUrl}/${endpoint}`, config).then(async (resp) => {
+    if (resp.status === 401) {
+      await logout();
+      window.location.reload();
+      return Promise.reject({ message: "重新登录" });
+    }
+    const data = await resp.json();
+    if (resp.ok) return data;
+    else return Promise.reject(data);
+  });
+};
