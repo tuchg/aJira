16557e3bc54887450243ff97babd1891ba9cce45
tuchg <ituchg@qq.com>
2022-05-22 16:28:20 +0800

    fix: useQueryParam循环依赖问题

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,15 +1,22 @@
 import { useSearchParams } from "react-router-dom";
+import { useMemo } from "react";
 
 /**
  * 返回页面URL中，指定键的参数值
  * @param keys
  */
-export const useQueryParam = (keys: string[]) => {
+export const useQueryParam = <K extends string>(keys: K[]) => {
+  // useState会解决对象实例比较问题
   const [searchParams, setSearchParams] = useSearchParams();
   return [
-    keys.reduce((prev, key) => {
-      return { ...prev, key: searchParams.get(key) || "" };
-    }, {} as { [key in string]: string }),
+    useMemo(
+      () =>
+        keys.reduce((prev, key) => {
+          return { ...prev, key: searchParams.get(key) || "" };
+        }, {} as { [key in K]: string }),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [searchParams]
+    ),
     setSearchParams,
     /**
      * as const 用于解决元组、函数解构等类型推断问题
