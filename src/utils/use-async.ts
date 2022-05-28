cac14bd72235dc4373fa7603e98ce19efeed22b9
tuchg <ituchg@qq.com>
2022-05-28 19:29:09 +0800

    refactor: useReducer改造useAsync

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 import { useMountedRef } from "./index";
 
 interface State<D> {
@@ -12,38 +12,52 @@ const defaultState: State<null> = {
   data: null,
   error: null,
 };
+
 const defaultConfig = {
   throwOnError: false,
 };
+
+/**
+ * 自动管理组件卸载赋值安全
+ * @param dispatch
+ */
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+  const mountedRef = useMountedRef();
+  return useCallback(
+    (...args: T[]) => (mountedRef.current ? dispatch(...args) : void 0),
+    [dispatch, mountedRef]
+  );
+};
+
 export const useAsync = <D>(
   initialState?: State<D>,
   initialConfig?: typeof defaultConfig
 ) => {
   const config = { ...defaultConfig, ...initialConfig };
-  const [state, setState] = useState<State<D>>({
-    ...defaultState,
-    ...initialState,
-  });
-  const mountedRef = useMountedRef();
+  const [state, dispatch] = useReducer(
+    (state: State<D>, action: Partial<State<D>>) => ({ ...state, ...action }),
+    { ...defaultState, ...initialState }
+  );
+  const safeDispatch = useSafeDispatch(dispatch);
   // useState传入函数的惰性初始化，会在初始化时执行一次，所以要用useState保存函数，不可直接传入函数
   const [retry, setRetry] = useState(() => () => {});
   const setData = useCallback(
     (data: D) =>
-      setState({
+      safeDispatch({
         data,
         status: "success",
         error: null,
       }),
-    []
+    [safeDispatch]
   );
   const setError = useCallback(
     (error: Error) =>
-      setState({
+      safeDispatch({
         error,
         status: "error",
         data: null,
       }),
-    []
+    [safeDispatch]
   );
 
   const run = useCallback(
@@ -52,14 +66,16 @@ export const useAsync = <D>(
         throw new Error("请传入 Promise 类型数据");
       }
       setRetry(() => () => {
-        if (runConfig) run(runConfig?.retry(), runConfig);
+        if (runConfig?.retry) run(runConfig?.retry(), runConfig);
       });
-      setState((prevState) => ({ ...prevState, status: "loading" }));
       /*  // 导致useCallback无限循环
-        setState({ ...state, status: "loading" });*/
+       * setState({ ...state, status: "loading" });
+       */
+      safeDispatch({ status: "loading" });
       return promise
         .then((data) => {
-          if (mountedRef.current) setData(data);
+          // if (mountedRef.current)
+          setData(data);
           return data;
         })
         .catch((err) => {
@@ -67,10 +83,10 @@ export const useAsync = <D>(
           if (config.throwOnError)
             // 直接return error的话 外层无法捕捉到
             return Promise.reject(err);
-          else return err;
+          return err;
         });
     },
-    [config.throwOnError, mountedRef, setData, setError]
+    [config.throwOnError, safeDispatch, setError, setData]
   );
 
   // const retry = () => {
